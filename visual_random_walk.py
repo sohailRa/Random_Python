89b53eaea269685b3e652735ba253204da0d87ac
sohailRa <sohail.ra5@gmail.com>
2017-01-07 21:16:34 -0500

    First

--- visual_random_walk.py
+++ visual_random_walk.py
@@ -4,7 +4,9 @@ from random_walk import RandomWalk
 while True:
 	r_walk = RandomWalk()
 	r_walk.walk()
-	plt.scatter(r_walk.x, r_walk.y, s=20)
+
+	point_nums = list(range(r_walk.num_points))
+	plt.scatter(r_walk.x, r_walk.y, c=point_nums, cmap=plt.cm.Blues, edgecolor='none',  s=20)
 	plt.show()
 
 	contin = raw_input("Another walk? (y/n): ")
