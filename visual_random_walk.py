1d66aa8afc96991a7126ab57e2dc46459a9038b9
sohailRa <sohail.ra5@gmail.com>
2017-01-07 21:12:34 -0500

    First

--- visual_random_walk.py
+++ visual_random_walk.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-r_walk = RandomWalk()
-r_walk.walk()
+while True:
+	r_walk = RandomWalk()
+	r_walk.walk()
+	plt.scatter(r_walk.x, r_walk.y, s=20)
+	plt.show()
 
-plt.scatter(r_walk.x, r_walk.y, s=20)
-plt.show()
+	contin = raw_input("Another walk? (y/n): ")
+	if contin == 'n':
+		break 
+		 
