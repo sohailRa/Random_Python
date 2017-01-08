585092adcadb16fd81325adbd97f67f109624249
sohailRa <sohail.ra5@gmail.com>
2017-01-07 21:38:52 -0500

    First

--- visual_random_walk.py
+++ visual_random_walk.py
@@ -7,8 +7,15 @@ while True:
 
 	point_nums = list(range(r_walk.num_points))
 	plt.scatter(r_walk.x, r_walk.y, c=point_nums, cmap=plt.cm.Reds, edgecolor='none',  s=20)
-	plt.show()
 
+	# Emphasize first and last points
+	plt.scatter(0, 0, c='blue', edgecolors='none', s=100)
+	plt.scatter(r_walk.x[-1], r_walk.y[-1], c='green', edgecolors='none', s=100)
+	# Remove the axes
+	plt.xticks([])
+	plt.yticks([])
+
+	plt.show()
 	contin = raw_input("Another walk? (y/n): ")
 	if contin == 'n':
 		break 
