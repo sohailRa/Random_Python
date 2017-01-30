80781e07f88813488e77ff9ff9bae91801481701
sohailRa <sohail.ra5@gmail.com>
2017-01-30 18:04:45 -0500

    First

--- coderByte_easy.py
+++ coderByte_easy.py
@@ -84,7 +84,31 @@ def TimeConvert(num):
     
 # keep this function call here  
 print TimeConvert(119)
+
+
 #------------------------------------------------------------------------
+# Challenge
+# Using the Python language, have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string.
+# Sample Test Cases
+
+# Input:"coderbyte"
+# Output:"bcdeeorty"
+
+# Input:"hooplah"
+# Output:"ahhloop"
+def AlphabetSoup(str): 
+
+  # convert the string into a list of characters
+  chars = list(str)
+
+  # sort the list in alphabetical order
+  sortedChars = sorted(chars)
+
+  # return the newly joined string
+  return "".join(sortedChars)
+    
+print AlphabetSoup("coderbyte")  
+
 
 #------------------------------------------------------------------------
 
